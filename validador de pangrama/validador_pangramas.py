e026556aefcde175efe34e37b24c35e65e07f024
Bruno Bento <brunoffbento@gmail.com>
2026-03-10 13:18:39 -0300

    restaurando organização

--- validador de pangrama/validador_pangramas.py
+++
@@ -1,9 +0,0 @@
-"""Validador de pangramas
-
-Um pangrama é uma frase que contém todas as letras do alfabeto."""
-
-
-def eh_pangrama(frase):
-    alfabeto = set('abcdefghijklmnopqrstuvwxyz')
-    letras_frase = set(frase.lower())
-    return alfabeto.issubset(letras_frase)
